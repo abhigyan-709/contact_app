dc1cdc8e1d2c36e807e731959df4f8646764666a
abhigyan-709 <abhigkumar709@gmail.com>
2025-07-15 20:23:05 +0530

    Version 1.0.1, Removed Old Code

--- frontend/contact-book/src/App.js
+++ frontend/contact-book/src/App.js
@@ -1,28 +1,3 @@
-// // src/App.js
-
-// import React, { useState } from "react";
-// import { Container, Typography } from "@mui/material";
-// import ContactForm from "./components/ContactForm";
-// import ContactList from "./components/ContactList";
-
-// function App() {
-//   const [refresh, setRefresh] = useState(false);
-
-//   const triggerRefresh = () => setRefresh((prev) => !prev);
-
-//   return (
-//     <Container maxWidth="md" sx={{ mt: 4 }}>
-//       <Typography variant="h4" gutterBottom>
-//         Contact Book
-//       </Typography>
-//       <ContactForm onContactAdded={triggerRefresh} />
-//       <ContactList refresh={refresh} />
-//     </Container>
-//   );
-// }
-
-// export default App;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ContactForm from "./components/ContactForm";
